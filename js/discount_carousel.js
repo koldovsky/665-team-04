10893739ba51fe03456129589ece8f87f1a0e7ef
Olexandr95 <alexandr.ruschak95@gmail.com>
2022-02-06 18:40:49 +0000

    add Js discount carousel

---
+++ js/discount_carousel.js
@@ -0,0 +1,48 @@
+(function() {
+    const slides = [
+                    `<div class="discount__position t-shirts" >
+                    <div class="discount__position-text"><p>
+                        20% OFF ALL T-SHIRTS
+                    </p>
+                    <a href="#"><p class="discount__link-text">Shop Collection →</p></a>    
+                    </div>
+                    </div>`,
+                    `<div class="discount__position jeckets">
+                    <div class="discount__position-text"><p>
+                        30% OFF ALL JACKETS
+                    </p>
+                    <a href="#"><p class="discount__link-text">Shop Collection →</p></a>    
+                    </div>
+                    </div>`,
+                    `<div class="discount__position trainers">
+                    <div class="discount__position-text"><p>
+                        25% OFF ALL TRAINERS
+                    </p>
+                    <a href="#"><p class="discount__link-text">Shop Collection →</p></a>    
+                    </div>
+                    </div>`
+
+    ];
+    
+    let currentSlideIdx = 0;
+    function showCurrentSlide() {
+        const slideContainer = document.querySelector(".discount__link_carusel_container");
+        slideContainer.innerHTML = slides[currentSlideIdx];
+        if (window.innerWidth >= 640) {
+            const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+            slideContainer.innerHTML += slides[secondSlideIdx];
+            if (window.innerWidth >= 900) {
+                const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+                slideContainer.innerHTML += slides[thirdSlideIdx];    
+            }
+        }
+    }
+    function showNextSlide(){
+    currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+    showCurrentSlide(); 
+        
+}
+    showCurrentSlide();
+    setInterval(showNextSlide, 3000);
+    window.addEventListener('resize', showCurrentSlide);
+})();
