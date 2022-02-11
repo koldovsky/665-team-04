1bb443d568337680d0c2f7ce93eaa762fadbc88e
Helen <lena@apserver.org.ua>
2022-02-11 17:12:53 +0200

    Changes fixed

--- js/new-arrivials-carousel.js
+++ js/new-arrivials-carousel.js
@@ -1,54 +1,54 @@
 (function () {
     const slides = [
-        `<div class="adidas__black__trainers">
-                      <img src="img/adidas-black-trainers.jpg" alt="adidas black trainers"
-                          class="new__arrivials__preview-img">
-                      <a class="new__arrivials__link__shop" href="#">
-                          <p class="new__arrivials__link">Adidas Black Trainers</p>
-                      </a>
-                      <p class="new__arrivials__price price">$60,00</p>
-                      <button class="new__arrivials__carousel__button__add">Add to Cart</button>
-                  </div>`,
-        `<div class="asics__jordan">
-                      <img src="img/asics-jordan-2000.jpg" alt="asics jordan 2000" class="new__arrivials__preview-img">
-                      <a class="new__arrivials__link__shop" href="#">
-                          <p class="new__arrivials__link">asics jordan 2000</p>
-                      </a>
-                      <p class="new__arrivials__price price">$200,00</p>
-                      <button class="new__arrivials__carousel__button__add">Add to Cart</button>
-                  </div>`,
-        `<div class="nike__winter__jacket">
-                      <img src="img/nike-winter-jacket-in-blue.jpg" alt="Nike Winter Jacket in Blue"
-                          class="new__arrivials__preview-img">
-                      <a class="new__arrivials__link__shop" href="#">
-                          <p class="new__arrivials__link">Nike Winter Jacket in Blue</p>
-                      </a>
-                      <p class="new__arrivials__price price">$85,00</p>
-                      <button class="new__arrivials__carousel__button__add">Add to Cart</button>
-                  </div>`,
-        `<div class="puma__textile__runnig">
-                      <img src="img/puma-textile-runnig-shoes.jpg" alt="Puma Textile Runnig Shoes"
-                          class="new__arrivials__preview-img">
-                      <a class="new__arrivials__link__shop" href="#">
-                          <p class="new__arrivials__link">Puma Textile Runnig Shoes</p>
-                      </a>
-                      <p class="new__arrivials__price price">$62,00</p>
-                      <button class="new__arrivials__carousel__button__add">Add to Cart</button>
-                  </div>`,
-        `<div class="reebok__red__dusters">
-                      <img src="img/reebok-red-duster.jpg" alt="Reebok Red Duster" class="new__arrivials__preview-img">
-                      <a class="new__arrivials__link__shop" href="#">
-                          <p class="new__arrivials__link">Reebok Red Duster</p>
-                      </a>
-                      <p class="new__arrivials__price price">$60,00</p>
-                      <button class="new__arrivials__carousel__button__add">Add to Cart</button>
-                  </div>`
+        `<div class="new__arrivials__product adidas__black__trainers">
+                    <img src="img/adidas-black-trainers.jpg" alt="adidas black trainers"
+                        class="new__arrivials__preview-img">
+                    <a class="new__arrivials__link__shop" href="#">
+                        <p class="new__arrivials__link">Adidas Black Trainers</p>
+                    </a>
+                    <p class="new__arrivials__price price">$60,00</p>
+                    <button class="new__arrivials__carousel__button__add">Add to Cart</button>
+                </div>`,
+        `<div class="new__arrivials__product asics__jordan">
+                    <img src="img/asics-jordan-2000.jpg" alt="asics jordan 2000" class="new__arrivials__preview-img">
+                    <a class="new__arrivials__link__shop" href="#">
+                        <p class="new__arrivials__link">asics jordan 2000</p>
+                    </a>
+                    <p class="new__arrivials__price price">$200,00</p>
+                    <button class="new__arrivials__carousel__button__add">Add to Cart</button>
+                </div>`,
+        `<div class="new__arrivials__product nike__winter__jacket">
+                    <img src="img/nike-winter-jacket-in-blue.jpg" alt="Nike Winter Jacket in Blue"
+                        class="new__arrivials__preview-img">
+                    <a class="new__arrivials__link__shop" href="#">
+                        <p class="new__arrivials__link">Nike Winter Jacket in Blue</p>
+                    </a>
+                    <p class="new__arrivials__price price">$85,00</p>
+                    <button class="new__arrivials__carousel__button__add">Add to Cart</button>
+                </div>`,
+        `<div class="new__arrivials__product puma__textile__runnig">
+                    <img src="img/puma-textile-runnig-shoes.jpg" alt="Puma Textile Runnig Shoes"
+                        class="new__arrivials__preview-img">
+                    <a class="new__arrivials__link__shop" href="#">
+                        <p class="new__arrivials__link">Puma Textile Runnig Shoes</p>
+                    </a>
+                    <p class="new__arrivials__price price">$62,00</p>
+                    <button class="new__arrivials__carousel__button__add">Add to Cart</button>
+                </div>`,
+        `<div class="new__arrivials__product reebok__red__dusters">
+                    <img src="img/reebok-red-duster.jpg" alt="Reebok Red Duster" class="new__arrivials__preview-img">
+                    <a class="new__arrivials__link__shop" href="#">
+                        <p class="new__arrivials__link">Reebok Red Duster</p>
+                    </a>
+                    <p class="new__arrivials__price price">$60,00</p>
+                    <button class="new__arrivials__carousel__button__add">Add to Cart</button>
+                </div>`
     ];
 
     let currentSlideIdx = 0;
 
     function showCurrentSlide() {
-        const slideContainer = document.querySelector(".new__arrivials__carousel__img");
+        const slideContainer = document.querySelector('.new__arrivials__carousel-slide-container');
         slideContainer.innerHTML = slides[currentSlideIdx];
 
         if (window.innerWidth >= 640) {
@@ -82,14 +82,9 @@
     document.querySelector('.new__arrivials__carousels__button__forward')
         .addEventListener('click', showNextSlide);
 
-    document.querySelector('.new__arrivials__carousels__button__forward')
-        .addEventListener('click', convertPrice);
-
     document.querySelector('.new__arrivials__carousels__button__back')
         .addEventListener('click', showPrevSlide);
 
-    document.querySelector('.new__arrivials__carousels__button__back')
-        .addEventListener('click', convertPrice);
 
     window.addEventListener('resize', showCurrentSlide);
 
