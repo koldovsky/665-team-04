3544f3b79cdf1ad062fd3c3325a8f2096c8e9fbe
Olha Bazhan <olyatarasevich2@gmail.com>
2022-02-13 23:48:01 +0200

    Added burger menu close icon

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,7 +1,14 @@
 (function () {
   const iconBurger = document.querySelector(".burger");
   const navigationsBody = document.querySelector(".navigate-wrapper");
+  const icon = document.querySelector("#burger");
   iconBurger.addEventListener("click", function () {
+    console.log(typeof(icon.src));
+  if(icon.src.includes("img/burger-close.png")) {
+    icon.src="img/burger-menu.png";
+  }else {
+    icon.src="img/burger-close.png";
+  }
     document.body.classList.toggle("lock");
     iconBurger.classList.toggle("active");
     navigationsBody.classList.toggle("active");
@@ -11,6 +18,7 @@
   navLinks.forEach((navLink) => {
     navLink.addEventListener("click", () => {
       if (iconBurger.classList.contains("active")) {
+        icon.src="img/burger-menu.png";
         document.body.classList.remove("lock");
         iconBurger.classList.remove("active");
         navigationsBody.classList.remove("active");
