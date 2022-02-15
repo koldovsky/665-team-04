7aa881e37add4fde430d3a96ace02f22c991a0e0
Helen <lena@apserver.org.ua>
2022-02-15 14:11:34 +0200

    The model of the form is complete.

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,16 +1,26 @@
 (function () {
-    const form = document.querySelector('.subscription__form');
+    let myModal = new bootstrap.Modal(document.getElementById('form__modal-subscription'),
+        {focus: true});
+
+    let textForUser = "Wait. Your data is being sent";
+
     async function handleSubmit(event) {
         event.preventDefault();
-        const data = new FormData(event.target);
-        fetch (even.target.action, {
+        let status = document.querySelector('#modal-text');
+        const form = document.querySelector('.subscription__form');
+        const data = new FormData(form);
+        status.innerText = "Wait! Your data is being sent";
+        await fetch(form.action, {
             method: form.method,
             body: data,
             headers: {
                 'Accept': 'application/json'
-            }
-        }).then ( response => {
-            form.reset()
-        })
+                }
+        });
+        form.reset();
+        status.innerText = "We will contact you shortly.";
+        myModal.show();
     }
+    document.querySelector('.subscription__button').addEventListener('click', handleSubmit);
+
 }) ();
